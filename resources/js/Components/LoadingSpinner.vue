3ec4a86cbd42b26f1367365a61d8b607acc62187
WyroneBlue <wyroneblue@gmail.com>
2022-02-23 19:18:34 +0100

    Create LoadingSpinner.vue

---
+++ resources/js/Components/LoadingSpinner.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<span class="w-20 h-20 border-b-2 border-gray-900 rounded-full animate-spin"></span>
+	</div>
+</template>
+
+<style scoped>
+	div{
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 40px 20px;
+		padding: 20px;
+	}
+</style>
