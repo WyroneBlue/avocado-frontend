71ea81b5d1bb4e52fab59a324a8c79790b88a828
WyroneBlue <wyroneblue@gmail.com>
2022-02-23 22:02:58 +0100

    added back button

--- resources/js/Pages/Film.vue
+++ resources/js/Pages/Film.vue
@@ -17,7 +17,7 @@
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-					<div class="container p-4 mx-auto mt-32 md:p-0">
+					<div class="container p-4 mx-auto my-32 md:p-0">
                         <LoadingSpinner v-if="state.isLoading"/>
 						<div class="flex flex-wrap w-full mx-auto shadow-lg lg:w-4/5" v-else-if="state.movieFound">
 
@@ -30,24 +30,27 @@
 							<div class="w-full bg-white md:w-1/2">
 								<div class="relative h-full px-6 mx-auto md:px-0 md:pt-6 md:-ml-6">
 									<div class="relative flex flex-wrap items-center p-6 mb-4 -mt-6 bg-white lg:h-full md:mt-0 md:mb-0 md:flex-wrap">
-										<div class="w-full text-center lg:w-1/5 lg:border-right lg:border-solid md:text-left">
+										<div class="w-full text-center lg:px-3 lg:border-right lg:border-solid md:text-left">
 											<h3>{{ state.movie.properties.title }} - episode: {{ state.movie.properties.episode_id }}</h3>
 											<p class="mt-3 mb-0 text-sm italic text-grey-dark">{{ state.movie.description }}</p>
+											<p class="mt-3 mb-0 text-sm italic text-grey-dark">produced by: {{ state.movie.properties.producer}}</p>
 											<p class="mt-3 mb-0 text-sm italic text-grey-dark">release date: {{ state.movie.properties.release_date }}</p>
 											<hr class="w-1/4 mt-4 border md:ml-0 lg:hidden">
 										</div>
 
-										<div class="w-full lg:w-3/5 lg:px-3">
+										<div class="w-full lg:px-3">
 											<p class="mt-4 text-sm text-justify text-md lg:mt-0 md:text-left">
 											{{ state.movie.properties.opening_crawl }}
 											</p>
 										</div>
 
-										<div class="w-full mt-6 text-center lg:w-1/5 lg:mt-0 lg:px-4 md:text-left">
-											<button class="w-1/3 py-2 bg-white border border-solid hover:bg-grey-darker hover:text-white border-grey lg:w-full">Visit now</button>
+										<div class="w-full mt-6 text-center lg:px-4 md:text-left">
+											<Link :href="route('db.films.index')" class="w-2/3 px-4 py-2 bg-white border border-solid hover:bg-grey-darker hover:font-black border-grey lg:w-full">
+												Back to overview
+											</Link>
 										</div>
 									</div>
-								</div>
+								</div>	
 							</div>
 						</div>
 						<div v-else>
@@ -105,9 +108,13 @@
 				.catch(error => { state.movieFound = true })
 			};
 
+			const goToOverview = () => {
+            	window.location = route('db.films.details', id);
+			}
+
 			getMovie();
 			
-			return { state, authUser }
+			return { state, authUser,  goToOverview}
 		}
 	}
 </script>
